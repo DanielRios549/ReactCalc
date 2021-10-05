3aa7b9c3f60c0775e00e3cb8293f9dd4888aee04
Daniel Rios <danielrios.flamengo@gmail.com>
2021-10-05 13:13:09 -0300

    Fix back button

--- src/components/calcs/common/expressions.ts
+++ src/components/calcs/common/expressions.ts
@@ -1,21 +1,33 @@
 export default class Expression {
     changeDisplay(display: string, event: React.MouseEvent): object {
         let data = event.currentTarget.getAttribute('data-display')
-        var newState = {}
+        let newState = {}
+        let current = ''
+
+        // Erase button
 
         if (data === 'C') {
             if (display.length === 1) {
-                newState = {current: "0"}
+                current = '0'
             }
-            
-            let remove = display.trim().split(' ').pop()
-
-            if (remove !== undefined) {
-                newState = {current: display.replace(remove, ' ').trim()}
+            else {
+                current = display.slice(0, -1)
             }
         }
+
+        // Expression buttons
+
         else {
-            newState = {current: `${display} ${data}`}
+            if (display === '0') {
+                current = `${data}`
+            }
+            else {
+                current = `${display}${data}`
+            }
+        }
+
+        newState = {
+            current: current
         }
 
         return newState
